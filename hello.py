a44a629a0ae641e8622d1049b5b0cccce278f5ba
bomiro <koalabxy@126.com>
2018-01-30 17:14:36 +0800

    add comment

--- hello.py
+++ hello.py
@@ -2,7 +2,10 @@
 import web  
 import sys
 sys.path.append(r'get_data/')
+
+#导入对应检索数据的文件
 import score, card, change2, borrow, library, probability
+#导入pyecharts模块显示所需要的包
 from pyecharts import Page, Timeline, Style, Overlap, Scatter, Gauge, \
                       Funnel, Bar, Line, Pie, Polar, Liquid
 
@@ -36,13 +39,15 @@ class blog:
                 stu_id = int(data['id']) #change to int 
                 print "type is :", type(data['id'])
                 #get_data file func
-                guage = score.get_score_data(stu_id)
-                funnel = card.all_month(stu_id)
-                bar_12month = card.month_sum(stu_id)
-                bar_eachmonth = card.each_month(stu_id)
-                pie = borrow.bookname(stu_id)
-                polar = library.librarytime(stu_id)
-                prediction = probability.prediction(stu_id)
+                guage = score.get_score_data(stu_id)            #提取成绩数据 展示成绩排名
+                funnel = card.all_month(stu_id)                 #提取一卡通消费数据 展示消费方式占比
+                bar_12month = card.month_sum(stu_id)            #提取一卡通消费数据 按消费方式展示花费金额
+                bar_eachmonth = card.each_month(stu_id)         #提取一卡通消费数据 统计每个月的花费总额 按照时间线展示
+                pie = borrow.bookname(stu_id)                   #提取图书馆借阅数据 展示阅读过的书籍
+                polar = library.librarytime(stu_id)             #提取图书馆门禁记录 记录去过图书馆的次数 按月显示
+                prediction = probability.prediction(stu_id)     #获取学号对应获得助学金的概率值 用概率显示
+
+                #打印提取出来的各项信息
                 print "------------------"
                 print guage ,funnel, bar_12month, bar_eachmonth, pie, polar, prediction
                 print "------------------"
@@ -53,11 +58,11 @@ class blog:
                                bar_eachmonth,
                                pie,
                                polar,
-                               prediction).render()
+                               prediction).render()             #生成显示网页render.html
 
-                return getid(stu_id, prediction)
+                return getid(stu_id, prediction)                #返回查找到的学号id 学院faculty 排名rank 概率prediction
             elif data['bt_type'] == "bt2":
-                return open(r'render.html').read()
+                return open(r'render.html').read()              #打开生成echarts的网页
 
 '''
 find student's info from id.csv
@@ -65,7 +70,7 @@ find student's info from id.csv
 def getid(name, pre):
     with open('result.csv') as f:
         list_id = f.readlines()
-        list_stu_id = [stu_id.strip().split(',') for stu_id in list_id]
+        list_stu_id = [stu_id.strip().split(',') for stu_id in list_id]#列表表达式 生成查询学号id的列表
         #print "*******************"
         #print "list_stu_id is", list_stu_id
         #print "*******************"
@@ -83,35 +88,42 @@ def getid(name, pre):
 
 '''
 create Echarts file
+下列参数都是通过学号查询相应文件查询出来的 格式为列表
 '''
 def create_echarts(gauge, funnel, bar_1, bar_2, pie, polar, prediction):
     page = Page()
     
+    #定义整体显示风格
     style = Style(
-        title_top='#fff',
-        title_pos='center',
-        width=1300,
-        height=600)
-        #background_color='#404a59')    
+        title_top='#fff',               #标题颜色
+        title_pos='center',             #标题位置: 中心
+        width=1300,                     #定义宽度
+        height=600)                     #定义高度
+        #background_color='#404a59')    #背景颜色
 
+    #显示欢迎图表 " Welcome "
     scatter = Scatter(**style.init_style)
     v1, v2 = scatter.draw("data/welcome.png")
     scatter.add("Welcome", v1, v2, is_random=True)
     page.add(scatter)
     
+    #应用仪表盘 显示该学生在此学院的排名情况
     charts = Gauge("成绩排名", **style.init_style)
     charts.add("", "学院排名", gauge[0], scale_range=[0, gauge[1]], 
                is_legend_show=True,
                is_random=True)
     page.add(charts)
     
+    #应用漏斗图 显示该学生消费方式的概况
     charts = Funnel("消费方式概况", **style.init_style)
     charts.add("消费方式", funnel[0], funnel[1], is_label_show=True, 
                label_pos='outside', legend_pos='left',
                legend_orient='vertical',
+               label_text_size=17,
                is_random=True)
     page.add(charts)
     
+    #应用饼图 显示该学生消费方式的占比情况
     charts = Pie("消费方式占比", **style.init_style)
     charts.add("消费方式", funnel[0], funnel[1],  is_random=True,
             radius=[15, 70], rosetype='radius', is_label_show=True, 
@@ -120,6 +132,7 @@ def create_echarts(gauge, funnel, bar_1, bar_2, pie, polar, prediction):
             label_text_size=17)
     page.add(charts)
 
+    #应用条形图 显示该学生在该学年每项消费方式额度的情况
     charts = Bar("消费方式(额度)展示", "全年 2013.9.1~2014.8.31", **style.init_style)
     charts.add("消费方式", funnel[0], funnel[1], 
                is_label_show=True, 
@@ -130,6 +143,7 @@ def create_echarts(gauge, funnel, bar_1, bar_2, pie, polar, prediction):
                )
     page.add(charts)    
     
+    #应用折线图 显示该学生在该学年每项消费方式额度的情况
     charts = Line("消费情况折线图", **style.init_style)
     charts.add("消费方式", funnel[0], funnel[1], 
              mark_point=["average", "max", "min"],
@@ -147,6 +161,7 @@ def create_echarts(gauge, funnel, bar_1, bar_2, pie, polar, prediction):
          area_opacity=0.3)
     page.add(charts)
     
+    #应用条形图(可伸缩, 可移动) 显示该学生该学年 每个月各项消费的总花费额
     attr = [month[0] for month in bar_1]
     values = [value[1] for value in bar_1]
     charts = Bar("学生每月消费水平", "每月", **style.init_style)
@@ -162,7 +177,7 @@ def create_echarts(gauge, funnel, bar_1, bar_2, pie, polar, prediction):
                is_random=True)
     page.add(charts)
 
-    
+    #套用时间线 应用条形图和折线图 综合显示该学生每月按照消费方式的消费情况
     timeline = Timeline(is_auto_play=True, timeline_bottom=0,width=1300,height=600)
     for month in bar_2:
         #title =  'month ' + str(month[0])
@@ -194,13 +209,15 @@ def create_echarts(gauge, funnel, bar_1, bar_2, pie, polar, prediction):
         timeline.add(overlap, 'month'+str(month[0]))
     page.add(timeline)
 
-    po = [i for i in polar[1]]
-    pi = [j for j in polar[0]]
+    #应用极坐标图 显示该学生12个月里 进入图书馆次数的情况
+    po = [i for i in polar[1]]          #列表表达式 生成月份
+    pi = [j for j in polar[0]]          #列表表达式 生成次数
     charts = Polar("进入图书馆的次数","2013.9 ~ 2014.8", **style.init_style)
     charts.add("", po, radius_data=pi,
           type='barRadius', is_random=True)
     page.add(charts)
 
+    #应用饼图 显示该学生所有阅读过的书籍信息
     charts = Pie("阅读过的书籍", **style.init_style)
     charts.add("", pie[0], pie[1], radius=[40, 75], label_text_color=None,
         is_label_show=True, legend_orient='vertical',is_legend_show=True,
@@ -210,7 +227,9 @@ def create_echarts(gauge, funnel, bar_1, bar_2, pie, polar, prediction):
 
     '''
     missing a prediction of grants
+    缺失正确的概率 需要套用模型计算
     '''
+    #应用流体图 显示该学生获得助学金的概率大小
     liquid = Liquid("获得助学金的概率", **style.init_style)
     liquid.add("", [prediction])
     page.add(liquid)        
